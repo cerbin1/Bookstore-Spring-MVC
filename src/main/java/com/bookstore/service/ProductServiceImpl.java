7bd8eaf8b88cad92b6fc68f5448ddbbdf264a572
cerbin <gekarez@gmail.com>
2018-03-30 21:26:20 +0200

    Move method getAllProducts to interface

---
+++ src/main/java/com/bookstore/service/ProductServiceImpl.java
@@ -0,0 +1,19 @@
+package com.bookstore.service;
+
+import com.bookstore.domain.Product;
+import com.bookstore.domain.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.getAllProducts();
+    }
+}
