f1f128988cdf08cd8ffddd38318b506a7308c59b
cerbin <gekarez@gmail.com>
2018-03-30 17:50:33 +0200

    Add configuration classes: DispatcherServlet, RootContext, WebContext

---
+++ src/main/java/com/bookstore/config/RootApplicationContextConfig.java
@@ -0,0 +1,27 @@
+package com.bookstore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("com.bookstore")
+public class RootApplicationContextConfig {
+    @Bean
+    public DataSource dataSource() {
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        return builder
+                .setType(EmbeddedDatabaseType.HSQL)
+                .build();
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate getJdbcTemplate() {
+        return new NamedParameterJdbcTemplate(dataSource());
+    }
+}
